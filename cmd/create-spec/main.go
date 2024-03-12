8bbc2a5d606934913b496ea662b17555a4fbc19c
呉山勝全 <kureyamayuri@katsuaki.local>
2024-03-12 10:44:23 +0900

    Add

--- cmd/create-spec/main.go
+++ cmd/create-spec/main.go
@@ -326,12 +326,12 @@ func generateSpec(config spec.Image, rootfs string) (_ *specs.Spec, err error) {
 		ctdoci.WithEnv(ic.Env),
 		ctdoci.WithTTY,           // TODO: make it configurable
 		ctdoci.WithNewPrivileges, // TODO: make it configurable
-		# ctdoci.WithDefaultUnixDevices
+		ctdoci.WithDefaultUnixDevices
 		ctdoci.WithAddedCapabilities([]string{"CAP_SYS_ADMIN", "CAP_NET_ADMIN"}),
 		ctdoci.WithAllKnownCapabilities,
 		ctdoci.WithAllDevicesAllowed 
-		# ctdoci.WithParentCgroupDevices
-		# ctdoci.WithWriteableSysfs 
+		ctdoci.WithParentCgroupDevices
+		ctdoci.WithWriteableSysfs 
 		//a,
 	//	ctdoci.WithPrivileged
 		
